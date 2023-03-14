c314fe6d440bc60243f33e0d50c172f668e6577d
JerryApplegarth <applegarth1630@gmail.com>
2023-03-14 05:08:05 -0700

    added picture and text

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,38 @@
+function fetchUser() {
+    fetch('./pictures.json')
+    .then((res) => res.json())
+    .then((data) => {
+        
+        displayerUser(data.results[0]);
+        
+    });
+}
 
+function displayerUser(user) {
+    const userDisplay = document.querySelector('#user');
+
+    // if(userDisplay.gender  === 'male') {
+    //     document.body.style.backgroundColor = 'blue';
+    // }else {
+    //     document.body.style.backgroundColor = 'pink';
+    // }
+    userDisplay.innerHTML = `
+        <div class= "flex flex-col justify-between">
+        <div class="flex flex-col text-center items-center">
+        <img src="${user.picture}" alt="picture of jerry applegarth" title="Jerry Applegarth"  >
+        <div class="space-y-3 space-x-3">
+        <p class="text-xl"><span class="font-bold">Name:</span> ${user.name}</p>
+        <p class="tex-xl"><span class="font-bold">Email:</span> ${user.email}</p>
+        <p class="tex-xl"><span class="font-bold">Cell:</span> ${user.cell}</p>
+        <p class="tex-xl"><span class="font-bold">Age:</span> ${user.age}</p>
+        
+    `;
+}
+
+document.querySelector('#generate').addEventListener('click', fetchUser);
+
+
+
+
+
+fetchUser();
